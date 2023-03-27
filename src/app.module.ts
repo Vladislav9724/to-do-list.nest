43f2d906eca335d44fa5f9dd05fe8c8103049fae
Vladislav9724 <vlad.tsyapka@gmail.com>
2023-03-27 20:51:58 +0300

    realise/configuration/environments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from "@nestjs/config";
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
-
-const uri = 'mongodb://localhost:27017';
+import configurations from "./configurations";
 
 @Module({
-  imports: [TasksModule, UsersModule, MongooseModule.forRoot(uri)],
+  imports: [ ConfigModule.forRoot({
+    isGlobal: true,
+    load: [configurations]
+    }),
+    TasksModule,
+    UsersModule,
+    MongooseModule.forRoot(process.env.MONGODB_URI)],
   controllers: [AppController],
   providers: [AppService],
 })
