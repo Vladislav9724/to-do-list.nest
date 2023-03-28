b60e188a2a45ec47cf4bf27142142fd8de404009
Vladislav9724 <vlad.tsyapka@gmail.com>
2023-03-28 21:35:09 +0300

    fix/configuration/environments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,22 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
-import configurations from "./configurations";
+import configurations from './configurations';
 
 @Module({
-  imports: [ ConfigModule.forRoot({
-    isGlobal: true,
-    load: [configurations]
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configurations],
     }),
     TasksModule,
     UsersModule,
-    MongooseModule.forRoot(process.env.MONGODB_URI)],
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
